043daf4f7a3f0a0b56ce17c86ea5909f19bab651
Taras Rodynenko <t.rodynenko@gmail.com>
2023-01-23 21:32:06 +0100

    pagehide both

--- public/index.js
+++ public/index.js
@@ -36,7 +36,7 @@ window.addEventListener(
   "pagehide",
   (event) => {
     const state = event.persisted ? "frozen" : "terminated";
-    // logNextState(state, "pagehide");
+    logNextState(state, "pagehide");
     navigator.sendBeacon('/log', new URLSearchParams({
       messege: `pagehide, ${state}`, id: 'pagehide'
     }))
